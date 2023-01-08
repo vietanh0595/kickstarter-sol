1aff9b4b0587577f72279024f1218b4a2a37bd1b
VA <duongvietanh0495@gmail.com>
2023-01-08 17:35:19 +0700

    redeploy contract

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -1,6 +1,6 @@
 import web3  from "./web3";
 import CampaignFactory from './build/CampaignFactory.json';
 
-const factory = new web3.eth.Contract(CampaignFactory.abi, '0x830555fB3373Db967FB64a1F1fB087a142262603');
+const factory = new web3.eth.Contract(CampaignFactory.abi, '0x1Ca6185b196D74c72dF6325650cE8BFC77188738');
 
 export default factory;
